f0a8f0481d94ac794cfd8ee63c82b0a4c3971c8b
pcapr <kowsik@gmail.com>
2011-04-18 10:40:14 -0700

    ruby 1.9 fixes

--- lib/blitz/command.rb
+++ lib/blitz/command.rb
@@ -2,13 +2,19 @@ require 'test/unit/assertions'
 
 # The default template string contains what was sent and received. Strip 
 # these out since we don't need them
-class Test::Unit::Assertions::AssertionMessage
-    alias :old_template :template
-    
-    def template
-        @template_string = ''
-        @parameters = []
-        old_template
+unless RUBY_VERSION =~ /^1.9/
+    class Test::Unit::Assertions::AssertionMessage
+        alias :old_template :template
+
+        def template
+            @template_string = ''
+            @parameters = []
+            old_template
+        end
+    end
+else
+    module ::Test::Unit
+        AssertionFailedError = MiniTest::Assertion
     end
 end
 
